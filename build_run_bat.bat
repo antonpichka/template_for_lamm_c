97744306b0b69376a74c5fab481f14a118adda0d
antonpichka <antonpichka@gmail.com>
2024-03-08 18:12:08 +0200

    refactoring(.): general rules LAMM

--- build_run_bat.bat
+++ build_run_bat.bat
@@ -7,6 +7,6 @@ rem ONLY EDIT THIS ROW (Name of your catalog)
 rd /s /q build
 rd /s /q %systemdrive%\$Recycle.bin
 Xcopy %project%\* build\ /S /E
-start /b named_bat_utility\copy_all_files_from_all_folders_to_one_folder_bat_utility.bat
-start /b named_bat_utility\remove_unnecessary_files_and_folders_for_the_build_bat_utility.bat
+start /b named_bat_utility\copy_all_files_w_all_folders_to_one_folder_bat_utility.bat
+start /b named_bat_utility\remove_unnecessary_files_w_folders_w_the_build_bat_utility.bat
 start /b named_bat_utility\dotnet_bat_utility.bat
